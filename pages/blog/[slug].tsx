9bfd4613ab142c65195b6739d95d981f45f6794e
unknown <rahulnakre@gmail.com>
2020-11-19 22:17:47 -0800

    actual blog page themeing for center

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,6 +3,10 @@ import Head from "next/head";
 import { getSinglePost } from "../api/posts";
 import { useRouter } from "next/router";
 import Container from "../../components/Container";
+import Link from "next/link";
+import { getPostInfoList } from "../api/posts";
+import { Stack, Flex, Heading, Box, Text, useColorMode } from "@chakra-ui/react";
+import PostLink from "../../components/PostLink";
 
 type PostProps = {
   htmlString: string,
@@ -27,13 +31,49 @@ const Post:FC<PostProps> = ({ ghostPost }) => {
 
   return (
     <Container>
-      <Head>
+      {/* <Head>
         <title>{ghostPost.title}</title>
-        {/* <meta title="description" content={ghostPost.metaDescription} /> */}
-      </Head>
-      <h1>{ghostPost.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: ghostPost.html }} />
+      </Head> */}
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        margin="0 auto 4rem auto"
+        maxWidth="700px"
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+          width="100%"
+        >
+          <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+            {ghostPost.title}
+          </Heading>
+
+        </Flex>
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+          width="100%"
+          marginTop={8}
+        >
+          <div dangerouslySetInnerHTML={{ __html: ghostPost.html }} />
+        </Flex>
+      </Stack>
     </Container>
+    // <Container>
+    //   <Head>
+    //     <title>{ghostPost.title}</title>
+    //     {/* <meta title="description" content={ghostPost.metaDescription} /> */}
+    //   </Head>
+    //   <h1>{ghostPost.title}</h1>
+    //   <div dangerouslySetInnerHTML={{ __html: ghostPost.html }} />
+    // </Container>
   );
 }
 
