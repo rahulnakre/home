6cc18808284d1481cc10ed394e694b57f469f289
rahulnakre <rahulnakre@gmail.com>
2022-12-31 02:43:39 -0500

    remove import

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import Link from "next/link";
 import Container from "../../components/Container";
 import { Stack, Flex, Heading } from "@chakra-ui/react";
 import PostLink from "../../components/PostLink";
